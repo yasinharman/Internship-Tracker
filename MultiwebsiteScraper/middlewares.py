2a54eda3ee7aef141feef1743b5f3c1a53612588
yasinharman <business.ysnhrmn@gmail.com>
2025-11-17 18:57:57 +0300

    Scrapy üzerinden UA değiştirmek için oluşturulan settings.py dosyasındaki UA listesi ve bu UA'ları rastgele bir şekilde rotate eden middleware silindi. Artık Scrapy ile Playwright arasında UA uyuşmazlığı yok.

--- MultiwebsiteScraper/middlewares.py
+++ MultiwebsiteScraper/middlewares.py
@@ -10,21 +10,6 @@ from itemadapter import ItemAdapter
 
 import random
 
-class RandomUserAgentMiddleware:
-    def __init__(self, user_agents):
-        self.user_agents = user_agents
-
-    @classmethod
-    def from_crawler(cls, crawler):
-        # settings.py içindeki listeyi alır
-        return cls(
-            user_agents=crawler.settings.get('USER_AGENT_LIST', [])
-        )
-
-    def process_request(self, request, spider):
-        if self.user_agents:
-            request.headers['User-Agent'] = random.choice(self.user_agents)
-
 class MultiwebsitescraperSpiderMiddleware:
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the spider middleware does not modify the
--- MultiwebsiteScraper/settings.py
+++ MultiwebsiteScraper/settings.py
@@ -17,19 +17,6 @@ NEWSPIDER_MODULE = "MultiwebsiteScraper.spiders"
 
 ADDONS = {}
 
-USER_AGENT_LIST = [
-"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
-"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0",
-"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 Edg/123.0.2420.81",
-"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 OPR/109.0.0.0",
-"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
-"Mozilla/5.0 (Macintosh; Intel Mac OS X 14.4; rv:124.0) Gecko/20100101 Firefox/124.0",
-"Mozilla/5.0 (Macintosh; Intel Mac OS X 14_4_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Safari/605.1.15",
-"Mozilla/5.0 (Macintosh; Intel Mac OS X 14_4_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 OPR/109.0.0.0",
-"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
-"Mozilla/5.0 (X11; Linux i686; rv:124.0) Gecko/20100101 Firefox/124.0"
-]
-
 DOWNLOAD_HANDLERS = { #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
     "http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
     "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
@@ -98,7 +85,6 @@ DOWNLOADER_MIDDLEWARES = {
 #     'scrapeops_scrapy.extension.ScrapeOpsMonitor': 500, #IMPORTANT FOR REQUEST MONITORING
 # }
 
-# SCRAPEOPS_API_KEY = "5fc28f18-f7df-41bc-bc79-fc41d3d3ad82"
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 #ITEM_PIPELINES = {
