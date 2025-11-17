aa3f1b5c705e113e326d219d823b31f0264a1c40
yasinharman <business.ysnhrmn@gmail.com>
2025-11-17 19:39:11 +0300

    headers.py dosyası kullanılmadığı için silindi.

--- MultiwebsiteScraper/spiders/headers.py
+++
@@ -1,9 +0,0 @@
-import scrapy
-
-class HeadersSpider(scrapy.Spider):
-    name = 'headers'
-    allowed_domains = ['httpbin.io']
-    start_urls = ['https://httpbin.io/user-agent']
-
-    def parse(self, response):
-        self.log(f'RESPONSE: {response.body}')
