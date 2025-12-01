1d19fc194c21a55a54226ef0dadfac495e4273a6
yasinharman <business.ysnhrmn@gmail.com>
2025-12-01 16:06:59 +0300

    indeed adlı site için oluşturulan spiderda database sütunları için gerekli verilerin çekilmesi adına gerekli css seçiciler oluşturuldu.

--- MultiwebsiteScraper/spiders/indeed_HTML_parse.py
+++ MultiwebsiteScraper/spiders/indeed_HTML_parse.py
@@ -5,8 +5,7 @@ class IndeedJobSpider(scrapy.Spider):
     name = "indeed_html"
 
     custom_settings = {
-        # LÜTFEN API KEY'İNİ AŞAĞIYA YAPIŞTIR
-        'SCRAPEOPS_API_KEY': '', 
+        'SCRAPEOPS_API_KEY': '9978eae2-0811-40b7-908d-28c38d5f4e3b', 
         'SCRAPEOPS_PROXY_ENABLED': True,
         'DOWNLOADER_MIDDLEWARES': {
             'scrapeops_scrapy_proxy_sdk.scrapeops_scrapy_proxy_sdk.ScrapeOpsScrapyProxySdk': 725,
@@ -15,16 +14,8 @@ class IndeedJobSpider(scrapy.Spider):
         'DOWNLOAD_DELAY': 2,
         
         'DEFAULT_REQUEST_HEADERS': {
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
                     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
                     'Accept-Language': 'tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7',
-                    'Accept-Encoding': 'gzip, deflate, br',
-                    'Referer': 'https://tr.indeed.com/',
-                    'Upgrade-Insecure-Requests': '1',
-                    'Sec-Fetch-Dest': 'document',
-                    'Sec-Fetch-Mode': 'navigate',
-                    'Sec-Fetch-Site': 'same-origin',
-                    'Sec-Fetch-User': '?1',
                 },
     }
 
@@ -54,7 +45,7 @@ class IndeedJobSpider(scrapy.Spider):
                             'location': location,
                             'offset': offset,
                             'page_num': page + 1,
-                            'sops_render_js': True, #####
+                            'sops_render_js': True, # İş ilanlarının listelendiği ilk sayfada bot korumasını bypass edebilmek için js render yapıyoruz.
                         }
                     )
     
@@ -71,29 +62,42 @@ class IndeedJobSpider(scrapy.Spider):
                     url = response.urljoin(link),
                     callback = self.parse_job_detail,
                     meta = {
-                        # 'sops_render_js': True,
-                        'keyword': response.meta.get('keyword') # Keyword verisini taşımaya devam et
+                        'sops_render_js': False, # İlan detaylarının olduğu sayfada antibot koruması çok yoğun olmadığından API kredisi tasarrufu ve hız için bu sayfalarda js render yapmıyoruz.
+                        'sops_residential': False,
+                        'sops_country': 'tr',
                     }
                 )
 
     def parse_job_detail(self, response):
         container = response.css("div.jobsearch-JobComponent")
 
-        job_title = container.css("h1[data-testid='jobsearch-JobInfoHeader-title']::text").get()
+        
+        job_title = container.css("h1[data-testid='jobsearch-JobInfoHeader-title'] span::text").get()
         
         company = container.css("div[data-testid='inlineHeader-companyName'] a::text").get()
         if not company:
             company = container.css("div[data-testid='inlineHeader-companyName']::text").get()
-            
+        
         location = container.css("div[data-testid='jobsearch-JobInfoHeader-companyLocation'] span::text").get()
         if not location:
              location = container.css("div[data-testid='jobsearch-JobInfoHeader-companyLocation']::text").get()
+        
+        text_nodes = response.xpath('//div[@id="jobDescriptionText"]//text()').getall()
+        job_description = " ".join([text.strip() for text in text_nodes if text.strip()])
+
+        source_site = "linkedin"
+
+
+
+
 
         yield {
-            'keyword': response.meta.get('keyword'),
             'job_title': job_title,
             'company': company,
             'location': location,
-            'url': response.url
+            "job_description": job_description,
+            'url': response.url,
+            "source_site": source_site,
+
         }
 
