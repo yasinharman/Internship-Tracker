da8f3d8b034d86545ccb81b43f5aa0d58609233c
yasinharman <business.ysnhrmn@gmail.com>
2025-12-09 13:36:59 +0300

    sqlalchemy ile oluşturulan models.py dosyası pipelines.py dosyasına entegre edilmek üzere eklendi.

---
+++ MultiwebsiteScraper/models.py
@@ -0,0 +1,35 @@
+from sqlalchemy import create_engine, Integer, String, Boolean, Column, Text, DateTime, ForeignKey
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
+from datetime import datetime
+
+Base = declarative_base()
+
+class JobPost(Base):
+    __tablename__ = "job_posts"
+    id = Column(Integer, primary_key=True)
+    
+    job_title = Column(String, nullable=False)
+    
+    company = Column(String)
+    
+    location = Column(String)
+    
+    job_description = Column(Text)
+    
+    url = Column(String, unique=True, nullable=False)
+    
+    source_site = Column(String, nullable=False)
+    
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    is_active = Column(Boolean, default=True)
+    
+    job_type = Column(String)
+
+Base.metadata.create_all()
+
+def db_connect():
+    return create_engine("postgresql+psycopg2://postgres:Mehperya16@localhost:5432/job_applications_db")
+
+def create_table(engine):
+    Base.metadata.create_all(engine)
