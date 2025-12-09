a3c16f75681d1a37b471c494e2a236905d4ed3ae
yasinharman <business.ysnhrmn@gmail.com>
2025-12-09 20:01:04 +0300

    Streamlit kullanılarak databaseden canlı olarak veri çeken bir dashboard oluşturuldu.

---
+++ app.py
@@ -0,0 +1,81 @@
+import streamlit as st
+import pandas as pd
+from sqlalchemy import create_engine
+from datetime import datetime, timedelta
+
+st.set_page_config(page_title="Job Applications Dasboard", layout="wide")
+
+@st.cache_data(ttl=600)
+def load_data():
+    db_connection_str = 'postgresql+psycopg2://postgres:Mehperya16@localhost:5432/job_applications_db'
+    db_connection = create_engine(db_connection_str)
+
+    query = "SELECT * FROM job_posts ORDER BY created_at DESC"
+    df = pd.read_sql(query, db_connection)
+
+    df['created_at'] = pd.to_datetime(df["created_at"])
+    return df
+
+try:
+    df = load_data()
+
+except Exception as e:
+    st.error(f"An error occured while connecting to database: {e}")
+    st.stop()
+
+
+st.sidebar.header("Filter")
+
+site_list = df["source_site"].unique().tolist()
+selected_sites = st.sidebar.multiselect("Source site", site_list, default=site_list)
+
+job_type_list = df["job_type"].unique().tolist()
+selected_job_types = st.sidebar.multiselect("Job type", job_type_list, default=job_type_list)
+
+filtered_df = df[df['source_site'].isin(selected_sites) & df['job_type'].isin(selected_job_types)]
+
+st.title("Current Job Postings")
+
+col1, col2, col3, = st.columns(3)
+
+total_jobs = len(filtered_df)
+col1.metric("Total Job Application Count", total_jobs)
+
+today = datetime.now().date()
+jobs_today = len(filtered_df[filtered_df['created_at'].dt.date == today])
+col2.metric("Added Today", jobs_today, delta=f"{jobs_today} new")
+
+total_companies = filtered_df["company"].nunique()
+col3.metric("Different Company Count", total_companies)
+
+st.markdown("---")
+
+col_chart1, col_chart2 = st.columns(2)
+
+with col_chart1:
+    st.subheader("Distribution by sites")
+    site_counts = filtered_df['source_site'].value_counts()
+    st.bar_chart(site_counts)
+
+with col_chart2:
+    st.subheader("Job Type")
+    if 'job_type' in filtered_df.columns and not filtered_df["job_type"].isnull().all():
+        type_counts = filtered_df['job_type'].value_counts()
+        st.bar_chart(type_counts)
+    
+    else:
+        st.info("There isn't any job applications with the selected job type.")
+    
+st.subheader("Recent Applications")
+
+display_columns = ["job_title", "company", "location", "source_site", "created_at", "job_type","url"]
+
+st.dataframe(
+    filtered_df[display_columns],
+    column_config={
+        "url": st.column_config.LinkColumn("Application URL"),
+        "created_at": st.column_config.DatetimeColumn("Date", format="D MMM YYYY, HH:mm")
+    },
+    width= "stretch",
+    hide_index=True
+)
